01ae37ebfbf970470392809244d613e92d4c1621
inesyaala <ines.benyaala@sesame.com.tn>
2023-08-09 18:34:05 +0100

    navbar implemented

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Contact us</li>
+        <li>About us</li>
+      </ul>
+    
+    </div>
+  );
+}
+
+export default Navbar;
